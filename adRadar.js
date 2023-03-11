109919e3ba58dbf2fa07bad1e2590da6104c07ed
Lauren <lhoughla@uccs.edu>
2023-03-11 11:18:58 -0700

    updated files

---
+++ adRadar.js
@@ -0,0 +1,10 @@
+/**
+ * requires: consts.js, config.js, log-event.js
+ */
+if ((constBool ==='true') && (isReady == true)) { main(); } else { console.warn(`[file] bgscr/special-case.js ==> %cERROR%c cannot find files: ${findMissingFiles()}`, "color:red", "color:transparant"); }
+function main() {
+	bgpa.console.log("[file] bgscr/ad-radar-advanced.js ==> %cREADY", "color:green");
+	if (switches.block.ads == true) {   BrowserAPI.webRequest.onBeforeRequest.addListener((d) => { logEvent(d, 'reg', 'ad-radar/ads'); return { cancel: true }; }, { urls: getReg() }, ["blocking"]); }
+	BrowserAPI.webRequest.onBeforeRequest.addListener((d) => { logEvent(d, 'adware', 'ad-radar/adware'); return { cancel: true }; }, { urls: getAdware() }, ["blocking"]);
+	if (switches.block.pings == true) { BrowserAPI.webRequest.onBeforeRequest.addListener((d) => { logEvent(d, 'ping', 'ad-radar/pings'); return { cancel: true }; }, { urls: ["*://*/*"], types: ["ping"] }, ["blocking"]); }
+}// End function main();
---
+++ consts.js
@@ -0,0 +1,23 @@
+/**
+ * global constants
+ */
+
+const constBool = 'true';
+// check if config.js, lists and reglist.js and lists/adware.js are found
+const isReady = (configBool && reglistBool && adwareBool) === 'true';
+
+// Browser API, usually universal
+const BrowserAPI = chrome
+
+// access background page, mostly used in console.log
+const bgpa = BrowserAPI.extension.getBackgroundPage();
+
+// console.log styling
+const ccss = {
+	succ: ["color:white; background-color:green;", "color:lightgreen; background-color:transparent;"],
+	fail: ["color:white; background-color:orangered;", "color:orangered; background-color:transparent;"],
+	adware: ["color:gray; background-color:yellow", "color:yellow; background-color:transparent;"],
+	warn: ["color:white; background-color:red;", "color:orangered; background-color:transparent;"],
+	info: ["color:white; background-color:cyan;", "color:cyan; background-color:transparent;"],
+	err: ["color:red"]
+};
